f6c7e77e8d34ee4ae0a7a0ec8b38a8cae56748bf
bmen25124 <bmen25124@gmail.com>
2025-03-22 06:12:53 +0300

    Added configuration elements

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ import {
   system_avatar,
   systemUserName,
 } from 'sillytavern-utils-lib/config';
-import { ChatMessage, EventNames, ExtractedData } from 'sillytavern-utils-lib/types';
+import { ChatCompletionMessage, ChatMessage, EventNames, ExtractedData } from 'sillytavern-utils-lib/types';
 import { POPUP_TYPE } from 'sillytavern-utils-lib/types/popup';
 
 const extensionName = 'SillyTavern-WorldInfo-Recommender';
@@ -19,14 +19,36 @@ const KEYS = {
   EXTENSION: 'worldInfoRecommender',
 } as const;
 
+interface ContextToSend {
+  lastMessages: boolean;
+  charCard: boolean;
+  authorNote: boolean;
+  worldInfo: boolean;
+}
+
 interface ExtensionSettings {
   version: string;
   formatVersion: string;
+  profileId: string;
+  maxContextType: 'profile' | 'sampler' | 'custom';
+  maxContextValue: number;
+  maxResponseToken: number;
+  contextToSend: ContextToSend;
 }
 
 const DEFAULT_SETTINGS: ExtensionSettings = {
   version: VERSION,
   formatVersion: FORMAT_VERSION,
+  profileId: '',
+  maxContextType: 'profile',
+  maxContextValue: 16384,
+  maxResponseToken: 1024,
+  contextToSend: {
+    lastMessages: true,
+    charCard: true,
+    authorNote: true,
+    worldInfo: true,
+  },
 };
 
 const settingsManager = new ExtensionSettingsManager<ExtensionSettings>(KEYS.EXTENSION, DEFAULT_SETTINGS);
@@ -53,11 +75,110 @@ async function handleUIChanges(): Promise<void> {
       large: true,
       wide: true,
     });
-  })
 
+    const settings = settingsManager.getSettings();
+    const container = $('#worldInfoRecommenderPopup');
+
+    globalContext.ConnectionManagerRequestService.handleDropdown(
+      '#worldInfoRecommenderPopup #worldInfoRecommend_connectionProfile',
+      settings.profileId,
+      (profile) => {
+        settings.profileId = profile?.id ?? '';
+        settingsManager.saveSettings();
+      },
+    );
+
+    const lastMessagesCheckbox = container.find('#worldInfoRecommend_lastMessages');
+    const charCardCheckbox = container.find('#worldInfoRecommend_charCard');
+    const authorNoteCheckbox = container.find('#worldInfoRecommend_authorNote');
+    const worldInfoCheckbox = container.find('#worldInfoRecommend_worldInfo');
+
+    lastMessagesCheckbox.prop('checked', settings.contextToSend.lastMessages);
+    charCardCheckbox.prop('checked', settings.contextToSend.charCard);
+    authorNoteCheckbox.prop('checked', settings.contextToSend.authorNote);
+    worldInfoCheckbox.prop('checked', settings.contextToSend.worldInfo);
+
+    lastMessagesCheckbox.on('change', () => {
+      settings.contextToSend.lastMessages = lastMessagesCheckbox.prop('checked');
+      settingsManager.saveSettings();
+    });
+    charCardCheckbox.on('change', () => {
+      settings.contextToSend.charCard = charCardCheckbox.prop('checked');
+      settingsManager.saveSettings();
+    });
+    authorNoteCheckbox.on('change', () => {
+      settings.contextToSend.authorNote = authorNoteCheckbox.prop('checked');
+      settingsManager.saveSettings();
+    });
+    worldInfoCheckbox.on('change', () => {
+      settings.contextToSend.worldInfo = worldInfoCheckbox.prop('checked');
+      settingsManager.saveSettings();
+    });
+
+    const maxContextType = container.find('#worldInfoRecommend_maxContextType');
+    const maxTokensContainer = container.find('#worldInfoRecommend_maxTokens_container');
+    maxContextType.val(settings.maxContextType);
+    maxTokensContainer.css('display', settings.maxContextType === 'custom' ? 'block' : 'none');
+    maxContextType.on('change', () => {
+      const value = maxContextType.val() as 'profile' | 'sampler' | 'custom';
+      settings.maxContextType = value;
+      settingsManager.saveSettings();
+      if (value === 'custom') {
+        maxTokensContainer.show();
+      } else {
+        maxTokensContainer.hide();
+      }
+    });
+
+    const maxTokens = container.find('#worldInfoRecommend_maxTokens');
+    maxTokens.val(settings.maxContextValue);
+    maxTokens.on('change', () => {
+      const value = maxTokens.val() as number;
+      settings.maxContextValue = value;
+      settingsManager.saveSettings();
+    });
+
+    const maxResponseTokens = container.find('#worldInfoRecommend_maxResponseTokens');
+    maxResponseTokens.val(settings.maxResponseToken);
+    maxResponseTokens.on('change', () => {
+      const value = maxResponseTokens.val() as number;
+      settings.maxResponseToken = value;
+      settingsManager.saveSettings();
+    });
+
+    container.find('#worldInfoRecommend_sendPrompt').on('click', async () => {
+      const prompt = container.find('#worldInfoRecommend_prompt').val();
+      if (!prompt) {
+        return;
+      }
+
+      if (!settings.profileId) {
+        return;
+      }
+
+      const messages: ChatCompletionMessage[] = [];
+      if (settings.contextToSend.lastMessages) {
+        const context = SillyTavern.getContext();
+        const profile = context.extensionSettings.connectionManager?.profiles?.find(
+          (profile) => profile.id === settings.profileId,
+        );
+        const presetName = profile?.preset;
+        const contextName = profile?.context;
+        const instructName = profile?.instruct;
+        const syspromptName = profile?.sysprompt;
+        messages.push(
+          ...(await buildPrompt(settings.profileId, undefined, undefined, {
+            presetName,
+            contextName,
+            instructName,
+            syspromptName,
+          })),
+        );
+      }
+    });
+  });
 }
-function initializeEvents() {
-}
+function initializeEvents() {}
 
 function stagingCheck(): boolean {
   if (!globalContext.ConnectionManagerRequestService) {
@@ -92,7 +213,10 @@ if (!stagingCheck()) {
     .catch((error) => {
       st_echo('error', error);
       globalContext.Popup.show
-        .confirm('Data migration failed. Do you want to reset the World Info Recommender data?', 'World Info Recommender')
+        .confirm(
+          'Data migration failed. Do you want to reset the World Info Recommender data?',
+          'World Info Recommender',
+        )
         .then((result) => {
           if (result) {
             settingsManager.resetSettings();
