ff66801d16a840fba68c0356fbd95fa3a5c27e4c
bmen25124 <bmen25124@gmail.com>
2025-03-22 04:19:54 +0300

    init

---
+++ src/index.ts
@@ -0,0 +1,88 @@
+import { buildPrompt, buildPresetSelect, ExtensionSettingsManager } from 'sillytavern-utils-lib';
+import {
+  characters,
+  selected_group,
+  st_echo,
+  st_runCommandCallback,
+  system_avatar,
+  systemUserName,
+} from 'sillytavern-utils-lib/config';
+import { ChatMessage, EventNames, ExtractedData } from 'sillytavern-utils-lib/types';
+
+const extensionName = 'SillyTavern-WorldInfo-Recommender';
+const VERSION = '0.1.0';
+const FORMAT_VERSION = 'F_1.0';
+const globalContext = SillyTavern.getContext();
+
+const KEYS = {
+  EXTENSION: 'worldInfoRecommender',
+} as const;
+
+interface ExtensionSettings {
+  version: string;
+  formatVersion: string;
+}
+
+const DEFAULT_SETTINGS: ExtensionSettings = {
+  version: VERSION,
+  formatVersion: FORMAT_VERSION,
+};
+
+const settingsManager = new ExtensionSettingsManager<ExtensionSettings>(KEYS.EXTENSION, DEFAULT_SETTINGS);
+
+async function handleUIChanges(): Promise<void> {
+  const settingsHtml: string = await globalContext.renderExtensionTemplateAsync(
+    `third-party/${extensionName}`,
+    'templates/settings',
+  );
+  $('#extensions_settings').append(settingsHtml);
+
+  const settingsContainer = $('.worldInfoRecommender_settings');
+
+  const settings = settingsManager.getSettings();
+}
+function initializeEvents() {
+}
+
+function stagingCheck(): boolean {
+  if (!globalContext.ConnectionManagerRequestService) {
+    return false;
+  }
+
+  if (!globalContext.getCharacterCardFields) {
+    return false;
+  }
+
+  if (!globalContext.getWorldInfoPrompt) {
+    return false;
+  }
+
+  return true;
+}
+
+function main() {
+  handleUIChanges();
+  initializeEvents();
+}
+
+if (!stagingCheck()) {
+  const errorStr = '[World Info Recommender Error] Make sure you are on staging branch and staging is updated.';
+  st_echo('error', errorStr);
+} else {
+  settingsManager
+    .initializeSettings()
+    .then((_result) => {
+      main();
+    })
+    .catch((error) => {
+      st_echo('error', error);
+      globalContext.Popup.show
+        .confirm('Data migration failed. Do you want to reset the World Info Recommender data?', 'World Info Recommender')
+        .then((result) => {
+          if (result) {
+            settingsManager.resetSettings();
+            main();
+          }
+        });
+    });
+}
